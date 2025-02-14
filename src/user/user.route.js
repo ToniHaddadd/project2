ad247a5ea8575f0c1cfcdc08257e10b1fb356f26
ToniHaddadd <tonih7930@gmail.com>
2025-02-14 20:04:46 +0200

    routes changed

---
+++ src/user/user.route.js
@@ -0,0 +1,8 @@
+import express from "express";
+// import userController from "./user.controler.js";
+const router=express.Router();
+
+router.post("/signUp",(req,res)=>{
+    console.log("hello");
+})
+export default router
